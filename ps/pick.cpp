bbd250c94ae27654433a03209e76629dbce1ee26
Seongjun Ahn <120538403+Gyvichi@users.noreply.github.com>
2025-03-11 21:08:18 +0900

    Create pick.cpp

---
+++ ps/pick.cpp
@@ -0,0 +1,8 @@
+//number of (x, y) : (0<=x<=n && 0<y<=k/d x + b/d)
+//argument should be positive
+
+ll count_solve(ll n, ll k, ll b, ll d){
+  if(!k) return (b/d)*n;
+  if (k >= d || b >= d) return ((k/d)*(n-1)+2*(b/d))*n/2+count_solve(n, k%d, b%d, d);
+  return count_solve((k*n+b)/d, d, (k*n+b)%d, k);
+}
